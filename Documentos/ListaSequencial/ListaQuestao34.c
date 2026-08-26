8f5eb72991db96bc422713fa25528659dde4bc3e
Geovana <maiageovana19@gmail.com>
2026-08-25 22:11:23 -0400

    Update ListaQuestao34.c

--- Documentos/ListaSequencial/ListaQuestao34.c
+++ Documentos/ListaSequencial/ListaQuestao34.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 int main(){
+    
     //Declaracao de variaveis
     float altura_degrau, altura_desejada;
     float quantidade_degraus;
+    
     //Entrada de dados
     printf("Digite a altura do degrau da escada: ");
     scanf("%f",&altura_degrau);
+   
     printf("Digite a altura que deseja atingir subindo a escada: ");
     scanf("%f",&altura_desejada);
+    
     //Processamento
     quantidade_degraus = (altura_desejada / altura_degrau);
+   
     //Saida de dados
     printf("Quantidade de graus devera subir para acalcar o objetivo: %.2f\n",quantidade_degraus);
     
