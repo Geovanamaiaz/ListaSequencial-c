09c821167c119f3fa0b33da27f3d4d6c7848eb55
Geovana <maiageovana19@gmail.com>
2026-08-25 22:10:29 -0400

    Update ListaQuestao08.c

--- Documentos/ListaSequencial/ListaQuestao08.c
+++ Documentos/ListaSequencial/ListaQuestao08.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
+    
     //Declaracao de variaveis
     float numero;
     float raiz_quadrada = 0;
+    
     //Entrada de dados
     printf("Digite um numero: ");
     scanf("%f",&numero);
+    
     //Processamento
     raiz_quadrada = sqrt(numero);
+    
     //Saida de dados
     printf("A raiz quadrada de %.2f: %.2f\n", numero, raiz_quadrada);
     
