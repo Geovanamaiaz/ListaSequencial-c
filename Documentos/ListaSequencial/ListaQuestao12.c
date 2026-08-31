4bcf2e152ca435d000b288289445378511ca4327
Geovana <maiageovana19@gmail.com>
2026-08-31 12:53:55 -0400

    Update ListaQuestao12.c

--- Documentos/ListaSequencial/ListaQuestao12.c
+++ Documentos/ListaSequencial/ListaQuestao12.c
@@ -12,7 +12,7 @@ int main(){
     novo_salario = salario_funcionario + (salario_funcionario * 0.25);
     
     //Saida de dados
-    printf("O novo salario do funcionario eh: %.2f\n",novo_salario);
+    printf("O novo salario do funcionario: %.2f\n",novo_salario);
     
     return 0;
 }
